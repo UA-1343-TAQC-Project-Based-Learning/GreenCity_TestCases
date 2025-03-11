e2ede9e86ec1a780220112b8cd4f86ffd5646734
Liubomyr Halamaha <galamagal86@gmail.com>
2025-03-11 15:47:11 +0200

    added a project skeleton

---
+++ src/main/java/com/greencity/ui/page/BasePage.java
@@ -0,0 +1,40 @@
+package com.greencity.ui.page;
+
+import com.greencity.ui.Base;
+import lombok.Getter;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Objects;
+
+
+@Getter
+public abstract class BasePage extends Base {
+
+
+    public BasePage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    private int getContentHeight() {
+        return ((Number) Objects.requireNonNull(threadJs.executeScript("return document.body.scrollHeight;"))).intValue();
+    }
+
+
+    public void waitForPageToLoad(long timeoutInSeconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds)).until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public Boolean isElementInvisible(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        return wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+
+}
---
+++ src/test/java/com/greencity/ui/BaseTest.java
@@ -0,0 +1,13 @@
+package com.greencity.ui;
+
+import com.greencity.ui.testrunners.BaseTestRunner;
+import org.testng.annotations.Test;
+
+public class BaseTest  extends BaseTestRunner {
+    @Test
+    public void firstTest() {
+        // TODO: Replace with actual test implementation
+        System.out.println("First test");
+        // Example of a meaningful assertion:
+    }
+}
