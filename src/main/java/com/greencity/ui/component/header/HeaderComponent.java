7f1fa9066903be4701bcbf080ab00a69f429d26e
Khrystyna <kristina50963@gmail.com>
2025-03-14 17:34:26 +0200

    add xpath

--- src/main/java/com/greencity/ui/component/header/HeaderComponent.java
+++ src/main/java/com/greencity/ui/component/header/HeaderComponent.java
@@ -13,7 +13,62 @@ public class HeaderComponent extends BaseComponent {
     @FindBy(xpath = ".//img[@src='assets/img/logo.svg']")
     private WebElement logo;
 
+    @Getter
+    @FindBy(xpath = ".//div[@class='header_navigation-menu']//a[contains(text(),'Еко новини')]")
+    private WebElement ecoNewsLink;
+
+    @Getter
+    @FindBy(xpath = ".//div[@class='header_navigation-menu']//a[contains(text(),'Події')]")
+    private WebElement eventLink;
+
+    @Getter
+    @FindBy(xpath = ".//div[@class='header_navigation-menu']//a[contains(text(),'Карта')]")
+    private WebElement placeLink;
+
+    @Getter
+    @FindBy(xpath = ".//div[@class='header_navigation-menu']//a[contains(text(),'Про нас')]")
+    private WebElement aboutUsLink;
+
+    @Getter
+    @FindBy(xpath = ".//div[@class='header_navigation-menu']//a[contains(text(),'Мій кабінет')]")
+    private WebElement myProfileLink;
+
+    @Getter
+    @FindBy(xpath = ".//div[@class='header_navigation-menu']//a[contains(text(),'UBS')]")
+    private WebElement ubsCourierLink;
+
+    @Getter
+    @FindBy(xpath = ".//img[@src='assets/img/search.svg']")
+    private WebElement searchIcon;
+
+    @Getter
+    @FindBy(xpath = ".//li[@class='lang-option']")
+    private WebElement languageSwitcherIcon;
+
+    @Getter
+    @FindBy(xpath = ".//li[@class='lang-option']")
+    private WebElement signInButton;
+
+    @Getter
+    @FindBy(xpath = ".//span[contains(text(),'Зареєструватися')]")
+    private WebElement registrationButton;
+
     public HeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+    }
+    public void clickEcoNews(){
+        ecoNewsLink.click();
+    }
+    public void clickEventLink(){
+        eventLink.click();
+    }
+    public void clickPlaceLink(){
+        placeLink.click();
+    }
+    public void clickAboutUsLink(){
+        aboutUsLink.click();
+    }
+    public void clickRegistrationButton(){
+        registrationButton.click();
     }
 }
---
+++ src/test/java/com/greencity/pages/Header.java
@@ -0,0 +1,6 @@
+package com.greencity.pages;
+
+public  abstract class Header {
+public static final String GREEN_CITY_LOGO = ".//div//img[@alt = 'Image green city logo']";
+public static final String SEARCH_ICON="$x(\"]";
+}
