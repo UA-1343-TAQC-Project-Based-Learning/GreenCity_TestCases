4bfdfb4128f933f6cb1cf59a431a55dea029e850
Maria-Markovych <mariamarkovich197@gmail.com>
2025-03-17 22:23:29 +0200

    some updates

--- src/main/java/com/greencity/ui/component/cards/NewsCardTableViewComponent.java
+++ src/main/java/com/greencity/ui/component/cards/NewsCardTableViewComponent.java
@@ -1,6 +1,6 @@
 package com.greencity.ui.component.cards;
 
-import com.greencity.ui.component.BaseComponent;
+
 
 import com.greencity.ui.page.newscardpage.NewsCardPage;
 import lombok.Getter;
@@ -8,99 +8,62 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-@Getter
-public class NewsCardTableViewComponent extends BaseComponent {
 
+public class NewsCardTableViewComponent extends NewsCardComponent {
+    @Getter
     @FindBy(xpath = ".//img[@class='list-image-content']")
     private WebElement image;
 
+    @Getter
     @FindBy(xpath = ".//div[@class='news-flags favourite-button']")
     private WebElement favouriteButton;
 
+    @Getter
     @FindBy(xpath = ".//div[contains(@class,'ul-eco-buttons')]/span")
     private List<WebElement> filtersTag;
 
+    @Getter
     @FindBy(xpath = ".//div[@class='title-list word-wrap']/h3")
     private WebElement titleLabel;
 
+    @Getter
     @FindBy(xpath = ".//div[contains(@class, 'list-text')]//span")
     private WebElement contentLabel;
 
+    @Getter
     @FindBy(xpath = ".//p[@class='user-data-text-date']//span")
     private WebElement dateOfCreationLabel;
 
+    @Getter
     @FindBy(xpath = ".//p[@class='user-data-text-date user']//span")
     private WebElement usernameLabel;
 
+    @Getter
     @FindBy(xpath = ".//img[@alt='comments']/following-sibling::span")
     private WebElement commentsLabel;
 
+    @Getter
     @FindBy(xpath = ".//img[@alt='likes']//following-sibling::span")
     private WebElement likesLabel;
 
     public NewsCardTableViewComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
-        check();
     }
 
-    private void check() {
-        if (image == null || favouriteButton == null || filtersTag == null || titleLabel == null
-                || contentLabel == null || dateOfCreationLabel == null || usernameLabel == null
-                || commentsLabel == null || likesLabel == null) {
-            throw new IllegalStateException("Some elements were not initialized properly");
-        }
-    }
-
-    public String getTitleLabelText() {
-        waitUntilElementVisible(titleLabel);
-        return titleLabel.getText();
-    }
-
-    public void titleLabelClick() {
+    private void titleLabelClick() {
         waitUntilElementClickable(titleLabel);
         titleLabel.click();
     }
 
-    public List<String> getFiltersTagText() {
-        waitUntilAllElementsVisible(filtersTag);
-        List<String> filtersTagNames = new ArrayList<>();
-        for (WebElement current : filtersTag) {
-            filtersTagNames.add(current.getText());
-        }
-        return filtersTagNames;
-    }
-
-    public String getContentLabelText() {
-        waitUntilElementVisible(contentLabel);
-        return contentLabel.getText();
-    }
-
-    public String getDateOfCreationLabelText() {
-        waitUntilElementVisible(dateOfCreationLabel);
-        return dateOfCreationLabel.getText();
-    }
-
-    public String getUsernameLabelText() {
-        waitUntilElementVisible(usernameLabel);
-        return usernameLabel.getText();
-    }
-
     public String getCommentsLabelText() {
-        waitUntilElementVisible(commentsLabel);
         return commentsLabel.getText();
     }
 
     public String getLikesLabelText() {
-        waitUntilElementVisible(likesLabel);
         return likesLabel.getText();
-    }
-
-    public void favouriteButtonClick() {
-        waitUntilElementClickable(favouriteButton);
-        favouriteButton.click();
     }
 
     public NewsCardPage openNewsCardPage() {
