f232f56a77231954b1af775a7e9467f296a6ef3f
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-03-18 15:07:31 +0200

    fixed xpath, deleted extra waiters

--- src/main/java/com/greencity/ui/component/EcoNewsFilterComponent.java
+++ src/main/java/com/greencity/ui/component/EcoNewsFilterComponent.java
@@ -1,6 +1,5 @@
 package com.greencity.ui.component;
 
-import com.greencity.ui.Base;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,31 +8,31 @@ import org.openqa.selenium.support.FindBy;
 public class EcoNewsFilterComponent extends BaseComponent {
 
     @Getter
-    @FindBy(xpath = "//span[contains(@class,'filter')]")
+    @FindBy(xpath = ".//span[contains(@class,'filter')]")
     private WebElement filterSpan;
 
     @Getter
-    @FindBy(xpath = "(//button[contains(@class, 'tag-button')]//span[contains(@class, 'text')])[1]")   // "//button//span[contains(text(),'Новини')]"
-    private WebElement newsFilterButton;
+    @FindBy(xpath = ".//button//span[contains(text(),'Новини') or contains(text(),'News')]")
+    private WebElement newsTagButton;
 
     @Getter
-    @FindBy(xpath = "(//button[contains(@class, 'tag-button')]//span[contains(@class, 'text')])[2]")
-    private WebElement eventsFilterButton;
+    @FindBy(xpath = ".//button//span[contains(text(),'Події') or contains(text(),'Events')]")
+    private WebElement eventsTagButton;
 
     @Getter
-    @FindBy(xpath = "(//button[contains(@class, 'tag-button')]//span[contains(@class, 'text')])[3]")
-    private WebElement educationFilterButton;
+    @FindBy(xpath = ".//button//span[contains(text(),'Освіта') or contains(text(),'Education')]")
+    private WebElement educationTagButton;
 
     @Getter
-    @FindBy(xpath = "(//button[contains(@class, 'tag-button')]//span[contains(@class, 'text')])[4]")
-    private WebElement initiativesFilterButton;
+    @FindBy(xpath = "(.//button//span[contains(text(),'Ініціативи')or contains(text(),'Initiatives')]")
+    private WebElement initiativesTagButton;
 
     @Getter
-    @FindBy(xpath = "(//button[contains(@class, 'tag-button')]//span[contains(@class, 'text')])[5]")
-    private WebElement adsFilterButton;
+    @FindBy(xpath = ".//button//span[contains(text(),'Реклама') or contains(text(),'Ads')]")
+    private WebElement adsTagButton;
 
-    @FindBy(xpath = "//app-remaining-count//h2")
-    private WebElement newsCountText;
+    @FindBy(xpath = ".//app-remaining-count//h2")
+    private WebElement newsCount;
 
 
 
@@ -43,63 +42,57 @@ public class EcoNewsFilterComponent extends BaseComponent {
 
 
     public String getFilterSpanText() {
-        waitUntilElementVisible(filterSpan);
         return filterSpan.getText();
     }
 
-    public String getNewsFilterText() {
-        waitUntilElementVisible(newsFilterButton);
-        return newsFilterButton.getText();
+    public String getNewsTagText() {
+        return newsTagButton.getText();
     }
 
-    public void clickNewsFilter() {
-        waitUntilElementClickable(newsFilterButton);
-        newsFilterButton.click();
+    public void clickNewsTag() {
+        waitUntilElementClickable(newsTagButton);
+        newsTagButton.click();
     }
 
-    public String getEventsFilterText() {
-        waitUntilElementVisible(eventsFilterButton);
-        return eventsFilterButton.getText();
+    public String getEventsTagText() {
+        return eventsTagButton.getText();
     }
 
-    public void clickEventsFilter() {
-        waitUntilElementClickable(eventsFilterButton);
-        eventsFilterButton.click();
+    public void clickEventsTag() {
+        waitUntilElementClickable(eventsTagButton);
+        eventsTagButton.click();
     }
 
-    public String getEducationFilterText() {
-        waitUntilElementVisible(educationFilterButton);
-        return educationFilterButton.getText();
+    public String getEducationTagText() {
+        return educationTagButton.getText();
     }
 
-    public void clickEducationFilter() {
-        waitUntilElementClickable(educationFilterButton);
-        educationFilterButton.click();
+    public void clickEducationTag() {
+        waitUntilElementClickable(educationTagButton);
+        educationTagButton.click();
     }
 
-    public String getInitiativesFilterText() {
-        waitUntilElementVisible(initiativesFilterButton);
-        return initiativesFilterButton.getText();
+    public String getInitiativesTagText() {
+        return initiativesTagButton.getText();
     }
 
-    public void clickInitiativesFilter() {
-        waitUntilElementClickable(initiativesFilterButton);
-        initiativesFilterButton.click();
+    public void clickInitiativesTag() {
+        waitUntilElementClickable(initiativesTagButton);
+        initiativesTagButton.click();
     }
 
-    public String getAdsFilterText() {
-        waitUntilElementVisible(adsFilterButton);
-        return adsFilterButton.getText();
+    public String getAdsTagText() {
+        return adsTagButton.getText();
     }
 
-    public void clickAdsFilter() {
-        waitUntilElementClickable(adsFilterButton);
-        adsFilterButton.click();
+    public void clickAdsTag() {
+        waitUntilElementClickable(adsTagButton);
+        adsTagButton.click();
     }
 
     public String getNewsCountText() {
-        waitUntilElementVisible(newsCountText);
-        return newsCountText.getText();
+        waitUntilElementVisible(newsCount);
+        return newsCount.getText();
     }
 
 
