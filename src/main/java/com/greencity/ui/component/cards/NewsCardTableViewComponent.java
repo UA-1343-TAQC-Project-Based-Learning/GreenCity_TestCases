a2ebe06e2a5cd013f57cb9a62e806826fa5f4362
Maria-Markovych <mariamarkovich197@gmail.com>
2025-03-18 20:30:32 +0200

    NewsCardTableViewComponent refactoring

--- src/main/java/com/greencity/ui/component/cards/NewsCardTableViewComponent.java
+++ src/main/java/com/greencity/ui/component/cards/NewsCardTableViewComponent.java
@@ -1,7 +1,7 @@
 package com.greencity.ui.component.cards;
 
 
-
+import com.greencity.ui.component.BaseComponent;
 import com.greencity.ui.page.newscardpage.NewsCardPage;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
@@ -9,10 +9,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class NewsCardTableViewComponent extends NewsCardComponent {
+public class NewsCardTableViewComponent extends BaseComponent {
     @Getter
     @FindBy(xpath = ".//img[@class='list-image-content']")
     private WebElement image;
@@ -49,8 +50,58 @@ public class NewsCardTableViewComponent extends NewsCardComponent {
     @FindBy(xpath = ".//img[@alt='likes']//following-sibling::span")
     private WebElement likesLabel;
 
+    private static final String CSS_PROPERTY_BACKGROUND = "background";
+
+    private static final String CSS_PROPERTY_DISPLAY = "display";
+
+    private static final String TAG_ATTRIBUTE_SRC = "src";
+
+    private static final String BACKGROUND_URL = "/assets/img/places/bookmark-set.svg";
+
     public NewsCardTableViewComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+    }
+
+    public String getTitleLabelText() {
+        return titleLabel.getText();
+    }
+
+    public List<String> getFiltersTagText() {
+        List<String> filtersTagNames = new ArrayList<>();
+        for (WebElement current : filtersTag) {
+            filtersTagNames.add(current.getText());
+        }
+        return filtersTagNames;
+    }
+
+    public String getContentLabelText() {
+        return contentLabel.getText();
+    }
+
+    public String getDateOfCreationLabelText() {
+        return dateOfCreationLabel.getText();
+    }
+
+    public String getUsernameLabelText() {
+        return usernameLabel.getText();
+    }
+
+    public NewsCardTableViewComponent favouriteButtonClick() {
+        waitUntilElementClickable(favouriteButton);
+        favouriteButton.click();
+        return this;
+    }
+
+    public String getImageSrc() {
+        return image.getAttribute(TAG_ATTRIBUTE_SRC);
+    }
+
+    public boolean isFavouriteButtonSelected() {
+        return favouriteButton.getCssValue(CSS_PROPERTY_BACKGROUND).contains(BACKGROUND_URL);
+    }
+
+    public boolean isImageDisplay() {
+        return !image.getCssValue(CSS_PROPERTY_DISPLAY).equals("none");
     }
 
     private void titleLabelClick() {
