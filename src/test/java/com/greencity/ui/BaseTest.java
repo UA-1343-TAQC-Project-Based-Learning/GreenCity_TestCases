c3c2c97e1687c948fc741e3c8de7fba961cc02f5
SerHemb <iserhiy888@gmail.com>
2025-03-18 16:08:27 -0500

    - updated

--- src/test/java/com/greencity/ui/BaseTest.java
+++ src/test/java/com/greencity/ui/BaseTest.java
@@ -1,16 +1,10 @@
 package com.greencity.ui;
 
 import com.greencity.ui.testrunners.BaseTestRunner;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.awt.event.WindowAdapter;
-import java.time.Duration;
 
 public class BaseTest  extends BaseTestRunner {
     @Test
