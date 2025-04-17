1eada0b576a885d9ce3654616b4ebcae0d1f1042
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-17 12:44:14 +0300

    Reduce duplication between methods

--- src/test/java/com/greencity/api/testRunners/EcoNewsClientTest.java
+++ src/test/java/com/greencity/api/testRunners/EcoNewsClientTest.java
@@ -7,9 +7,11 @@ import com.greencity.utils.TestValueProvider;
 import io.restassured.response.Response;
 import lombok.SneakyThrows;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import java.util.Collections;
+import java.util.List;
 
 
 import static com.greencity.api.testRunners.ApiTestRunner.testValueProvider;
@@ -41,62 +43,44 @@ public class EcoNewsClientTest {
         System.out.println("Testing with news ID: " + KNOWN_EXISTING_NEWS_ID);
     }
 
-    @SneakyThrows
-    @Test
-    public void getEcoNewsByIdForEnLanguage() {
-        Response response = ecoNewsClient.getEcoNewsByIdRawResponse(KNOWN_EXISTING_NEWS_ID, LANG_EN);
-        System.out.println("Request URL: " + response.getDetailedCookies());
+    @Test(dataProvider = "languageProvider")
+    public void getEcoNewsById(String language, List<String> expectedTags) {
+        Response response = ecoNewsClient.getEcoNewsByIdRawResponse(KNOWN_EXISTING_NEWS_ID, language);
+        System.out.println("Response cookies: " + response.getDetailedCookies());
         response.then().log().all();
 
         response.then().statusCode(200);
         EcoNews ecoNews = response.as(EcoNews.class);
 
-        String creationDateRaw = ecoNews.getCreationDate().toString();
-        String actualDateStr = DateUtils.convertRawDateToIso(creationDateRaw);
+        verifyEcoNews(ecoNews, language.equals(LANG_UK) ? ecoNews.getTagsUa() : ecoNews.getTags(), expectedTags);
+    }
 
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(actualDateStr, EXPECTED_CREATION_DATE_PART,
-                "Date mismatch. Actual: " + actualDateStr + ", Expected: " + EXPECTED_CREATION_DATE_PART);
-        softAssert.assertEquals((long) ecoNews.getId(), EXPECTED_NEWS_ID, "News ID mismatch");
-        softAssert.assertEquals(ecoNews.getTitle(), EXPECTED_TITLE, "Title mismatch");
-        softAssert.assertEquals(ecoNews.getContent(), EXPECTED_CONTENT, "Content mismatch");
-        softAssert.assertEquals(ecoNews.getTags(), Collections.singletonList("News"), "Tag mismatch");
-        softAssert.assertEquals((long) ecoNews.getAuthor().getId(), EXPECTED_AUTHOR_ID, "Author ID mismatch");
-        softAssert.assertEquals(ecoNews.getAuthor().getName(), EXPECTED_AUTHOR_NAME, "Author name mismatch");
-        softAssert.assertEquals((int) ecoNews.getLikes(), EXPECTED_LIKES, "Likes count mismatch");
-        softAssert.assertEquals((int) ecoNews.getDislikes(), EXPECTED_DISLIKES, "Dislikes count mismatch");
-        softAssert.assertEquals((int) ecoNews.getCountComments(), EXPECTED_COMMENTS, "Comments count mismatch");
-        softAssert.assertEquals(ecoNews.getHidden().booleanValue(), EXPECTED_HIDDEN, "News should not be hidden");
-
-        softAssert.assertAll();
+    @DataProvider
+    public Object[][] languageProvider() {
+        return new Object[][] {
+                {LANG_EN, Collections.singletonList("News")},
+                {LANG_UK, Collections.singletonList("Новини")}
+        };
     }
 
     @SneakyThrows
-    @Test
-    public void getEcoNewsByIdForUaLanguage() {
-        Response response = ecoNewsClient.getEcoNewsByIdRawResponse(KNOWN_EXISTING_NEWS_ID, LANG_UK);
-        System.out.println("Request URL: " + response.getDetailedCookies());
-        response.then().log().all();
-
-        response.then().statusCode(200);
-        EcoNews ecoNews = response.as(EcoNews.class);
-
+    private void verifyEcoNews(EcoNews ecoNews, List<String> actualTags, List<String> expectedTags) {
         String creationDateRaw = ecoNews.getCreationDate().toString();
         String actualDateStr = DateUtils.convertRawDateToIso(creationDateRaw);
 
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(actualDateStr, EXPECTED_CREATION_DATE_PART,
                 "Date mismatch. Actual: " + actualDateStr + ", Expected: " + EXPECTED_CREATION_DATE_PART);
-        softAssert.assertEquals((long) ecoNews.getId(), EXPECTED_NEWS_ID, "News ID mismatch");
+        softAssert.assertEquals(ecoNews.getId().longValue(), EXPECTED_NEWS_ID, "News ID mismatch");
         softAssert.assertEquals(ecoNews.getTitle(), EXPECTED_TITLE, "Title mismatch");
         softAssert.assertEquals(ecoNews.getContent(), EXPECTED_CONTENT, "Content mismatch");
-        softAssert.assertEquals(ecoNews.getTagsUa(), Collections.singletonList("Новини"), "UA Tag mismatch");
-        softAssert.assertEquals((long) ecoNews.getAuthor().getId(), EXPECTED_AUTHOR_ID, "Author ID mismatch");
+        softAssert.assertEquals(actualTags, expectedTags, "Tag mismatch");
+        softAssert.assertEquals(ecoNews.getAuthor().getId().longValue(), EXPECTED_AUTHOR_ID, "Author ID mismatch");
         softAssert.assertEquals(ecoNews.getAuthor().getName(), EXPECTED_AUTHOR_NAME, "Author name mismatch");
-        softAssert.assertEquals((int) ecoNews.getLikes(), EXPECTED_LIKES, "Likes count mismatch");
-        softAssert.assertEquals((int) ecoNews.getDislikes(), EXPECTED_DISLIKES, "Dislikes count mismatch");
-        softAssert.assertEquals((int) ecoNews.getCountComments(), EXPECTED_COMMENTS, "Comments count mismatch");
-        softAssert.assertEquals(ecoNews.getHidden().booleanValue(), EXPECTED_HIDDEN, "News should not be hidden");
+        softAssert.assertEquals(ecoNews.getLikes().intValue(), EXPECTED_LIKES, "Likes count mismatch");
+        softAssert.assertEquals(ecoNews.getDislikes().intValue(), EXPECTED_DISLIKES, "Dislikes count mismatch");
+        softAssert.assertEquals(ecoNews.getCountComments().intValue(), EXPECTED_COMMENTS, "Comments count mismatch");
+        softAssert.assertEquals(ecoNews.getHidden(), Boolean.valueOf(EXPECTED_HIDDEN), "News should not be hidden");
 
         softAssert.assertAll();
     }
